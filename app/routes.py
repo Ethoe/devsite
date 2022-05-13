d13343767d392c515cfdfee3e8b96971e52bb2b7
ethoe <ryanouttrim@gmail.com>
2022-05-12 19:21:16 -0700

    adding templating

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,9 @@
+from flask import render_template
 from app import app
 
+
 @app.route('/')
+@app.route('/index')
 def index():
-	return "whatsup"
+    user = {'username': 'Memer'}
+    return render_template('index.html', title="Home", user=user)
---
+++ run.ps1
@@ -0,0 +1,2 @@
+$env:FLASK_APP = "ethoeblog.py"
+flask run
