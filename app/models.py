5ab1ce26ebafb7c253e514d7cb0b162f4fdd6ba7
ethoe <ryanouttrim@gmail.com>
2022-05-14 16:10:44 -0700

    inital db additions

---
+++ app/models.py
@@ -0,0 +1,23 @@
+from datetime import datetime
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+
+    def __repr__(self):
+        return '<ID: {0}, User: {1}, Email: {2}>'.format(self.id, self.username, self.email)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self) -> str:
+        return '<Author: {0}, Post: {1}'.format(self.user_id, self.body)
